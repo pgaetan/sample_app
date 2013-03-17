d7f35178b0dcf006671d23c5368ba7873f3f5d90
pgaetan <nagao@hotmail.fr>
2013-03-17 17:36:39 +0100

    Fin du layout et des routages

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -21,5 +21,10 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Aide")
   end
+
+  it "devrait avoir une page d'inscription a '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Inscription")
+  end
 end
 
