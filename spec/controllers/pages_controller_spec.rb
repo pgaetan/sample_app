e0afc65655ae4a7c5f8a27adea15f697abd5e54c
pgaetan <nagao@hotmail.fr>
2013-03-17 15:52:14 +0100

    Suppression fichier backup

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,49 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+
+  before(:each) do
+    @base_title = "Simple App du Tutoriel Ruby on Rails"
+  end
 
   describe "GET 'home'" do
-    it "should be successful" do
+    it "devrait réussir" do
       get 'home'
       response.should be_success
+    end
+
+    it "devrait avoir le bon titre" do
+      get 'home'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Home")
     end
   end
 
   describe "GET 'contact'" do
-    it "should be successful" do
+    it "devrait réussir" do
       get 'contact'
       response.should be_success
     end
+
+    it "devrait avoir le bon titre" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Contact")
+    end
   end
 
+  describe "GET 'about'" do
+    it "devrait réussir" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "devrait avoir le bon titre" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => @base_title + " | About")
+    end
+  end
 end
+
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+
+  it "devrait trouver une page Accueil a '/' " do
+    get '/'
+    response.should have_selector('title', :content => "Accueil")
+  end
+
+  it "devrait trouver une page Contact a '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => "Contact")
+  end
+
+  it "devrait trouver une page A Propos page a '/about'" do
+    get '/about'
+    response.should have_selector('title', :content => "A Propos")
+  end
+
+  it "devrait trouver une page aide a '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => "Aide")
+  end
+end
+
